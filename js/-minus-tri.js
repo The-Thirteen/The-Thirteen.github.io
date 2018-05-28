b310fc3dae4a1162554303bbcd37bb1b50ae59e9
AcNEO <37327658+AcNEO@users.noreply.github.com>
2018-05-28 10:11:52 +0800

    :thinking:

---
+++ js/-minus-tri.js
@@ -0,0 +1,18 @@
+
+function validateNotBOT() {
+    var user;
+    this.user = function getUser() {
+        user = "user";
+        return user;
+    }
+    this.attempt = function attempt() {
+        user = getUser();
+        var attempt;
+        return attempt;
+    }
+    var validate = false;
+    if(validate = false) {
+        var timeout = 60*100; // timeout
+        
+    }
+}
